0b9932165ef9dc27a0d5c08142100a510513b76e
Schlegen <nicolas.schlegel@eleves.enpc.fr>
2019-04-08 14:40:46 +0200

    Ajout de code.py

---
+++ code.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  8 14:17:06 2019
+
+@author: Nicolas SCHLEGEL
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Acteur:
+    def __init__(self, dem,prod):
+        self.tab_demande = i
+        self.tab_production = prod
