018f5612d88ccc525c106a915c7b1dd020ae8176
Schlegen <nicolas.schlegel@eleves.enpc.fr>
2019-04-14 10:28:22 +0200

    projet fini, code principal et graphiques, ouvrir avec anaconda

---
+++ graphics.py
@@ -0,0 +1,203 @@
+# The code for changing pages was derived from: http://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter
+# License: http://creativecommons.org/licenses/by-sa/3.0/	
+
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+import tkinter as tk
+from tkinter import ttk
+from matplotlib.patches import Rectangle
+from matplotlib.collections import PatchCollection
+
+
+LARGE_FONT= ("Verdana", 12)
+
+callfunctions={"building": (lambda t: squarecall(t,8,18,10)) , "data_center": (lambda t: randomcall(t,7,3)), "PV_production": (lambda t:gaussiancall(t,12,2,-16)),"VE": (lambda t: randomcall(t,0,5))   }        
+names=[ "building", "data_center", "PV_production", "VE"]
+manager=Manager(names,callfunctions)        
+manager.simulation(24)      
+
+def plotload(player,ax):
+    rects=[]
+    for t in range(len(player.load)):
+        rect= Rectangle( (t,0),1,player.load[t] )
+        rects.append(rect)
+    # Create patch collection with specified colour/alpha
+    pc = PatchCollection(rects, alpha=0.5, facecolor="grey", edgecolor="black")
+    # Add collection to axes
+    ax.add_collection(pc)
+    ax.set_xlim(0,len(player.load))
+    ax.set_ylim(1.2*min(0,min(player.load)),1.2*max(player.load))
+    ax.grid(True)
+    ax.set_title(player.name+": load")
+        
+        
+def plotbills(player,ax):
+    rects=[]
+    for t in range(len(player.bill)):
+        rect= Rectangle( (t,0), 1, player.bill[t] )
+        rects.append(rect)
+    # Create patch collection with specified colour/alpha
+    pc = PatchCollection(rects, alpha=0.5, facecolor="green", edgecolor="black")
+    # Add collection to axes
+    ax.add_collection(pc)
+    ax.set_xlim(0,len(player.bill))
+    ax.set_ylim(1.2*min(0,min(player.bill)),1.2*max(0,max(player.bill)))
+    ax.grid(True)
+    ax.set_title(player.name+": bill")
+                
+def plotsupplied(player, ax):
+    rects=[]
+    for t in range(len(player.supplied)):
+        rect= Rectangle( (t,0), 1, player.supplied[t] )
+        rects.append(rect)
+    # Create patch collection with specified colour/alpha
+    pc = PatchCollection(rects, alpha=0.5, facecolor="blue", edgecolor="black")
+    ax.add_collection(pc)
+    ax.set_xlim(0,len(player.supplied))
+    ax.set_ylim(1.2*min(0,min(player.supplied)),1.2*max(0,max(player.supplied)))
+    ax.grid(True)
+    ax.set_title(player.name+": supplied load")
+    
+def plotbalance(manager,ax):                         
+    rects=[]
+    for t in range(len(manager.balance)):
+        rect= Rectangle( (t,0), 1, manager.balance[t] )
+        rects.append(rect)
+        # Create patch collection with specified colour/alpha
+    pc = PatchCollection(rects, alpha=0.5, facecolor="orange", edgecolor="black")
+        # Add collection to axes
+    ax.add_collection(pc)
+    ax.set_xlim(0,len(manager.balance))
+    ax.set_ylim(1.2*min(0,min(manager.balance)),1.2*max(0,max(manager.balance)))
+    ax.grid(True)
+    ax.set_xlabel("Global balance")    
+       
+class SeaofBTCapp(tk.Tk):
+
+    def __init__(self, manager, *args, **kwargs):
+        
+        tk.Tk.__init__(self, *args, **kwargs)
+
+       # tk.Tk.iconbitmap(self, default="clienticon.ico")
+        tk.Tk.wm_title(self, "Sea of BTC client")
+        
+        
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand = True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        
+        # first page
+        frame= StartPage(container, self, manager)        
+        self.frames["StartPage"] = frame
+        frame.grid(row=0, column=0, sticky="nsew")
+        
+        
+        #page for the manager
+        frame= PageManager(container, self, manager)        
+        self.frames["Manager"] = frame
+        frame.grid(row=0, column=0, sticky="nsew")
+        
+        #pages for the players
+        for name in manager.players:
+            
+            frame= PagePlayer(container, self, manager.players[name])   
+            print("ajout", name)
+            self.frames[name] = frame      
+            frame.grid(row=0, column=0, sticky="nsew")
+            
+        for key in self.frames: print("key",key, self.frames[key])            
+        self.show_frame("StartPage")
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        print("affichage", cont)
+        frame.tkraise()
+
+        
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller, manager):
+        
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self, text="Who are you?", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button = ttk.Button(self, text="Manager",
+                            command=lambda: controller.show_frame("Manager"))
+        button.pack()
+
+        names=[]
+        i=-1
+        for name in manager.players:
+            print("bouton",name)
+            names.append(name)
+            i+=1
+            button=ttk.Button(self, text= name, command = lambda toto= i: controller.show_frame(names[toto])) ######## ici astuce 
+            button.pack(fill=None)  
+            
+            
+            
+class PageManager(tk.Frame):
+
+    def __init__(self, parent, controller, manager):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Manager", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = ttk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame("StartPage"))
+        button1.pack()
+
+        f = Figure(figsize=(8,4), dpi=100)
+        a = f.add_subplot(111)
+        plotbalance(manager, a)
+
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2Tk(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+
+
+class PagePlayer(tk.Frame):
+
+    def __init__(self, parent, controller, player):
+        tk.Frame.__init__(self, parent)
+        print("remplissage_page", player.name)
+        label = tk.Label(self, text=player.name, font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = ttk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame("StartPage"))
+        button1.pack()
+
+        f = Figure(figsize=(20,8), dpi=100)
+        a = f.add_subplot(221)
+        plotload(player,a)
+
+        a2 = f.add_subplot(222)
+        plotsupplied(player,a2)
+
+        a3 = f.add_subplot(223)
+        plotbills(player,a3)
+
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2Tk(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        
+
+app = SeaofBTCapp(manager)
+app.mainloop()
--- maincode.py
+++ maincode.py
@@ -1,11 +1,8 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from random import randint
 import math as mh
-from matplotlib.patches import Rectangle
-from matplotlib.collections import PatchCollection
 
-class Player:
+class Player:  
     def __init__(self,load,bill,callfunction,supplied,name):
         self.load = load # négatif si on produit
         self.bill =  bill # positif si on produit
@@ -13,55 +10,7 @@ class Player:
         self.supplied =supplied
         self.name=name
     def call(self, t):
-        self.load.append(self.callfunction(t+.5))
-        
-    def plotload(self):
-        fig, ax = plt.subplots(1)
-        rects=[]
-        for t in range(len(self.load)):
-            rect= Rectangle( (t,0),1,self.load[t] )
-            rects.append(rect)
-        # Create patch collection with specified colour/alpha
-        pc = PatchCollection(rects, alpha=0.5, facecolor="grey", edgecolor="black")
-        # Add collection to axes
-        ax.add_collection(pc)
-        ax.set_xlim(0,len(self.load))
-        ax.set_ylim(1.2*min(0,min(self.load)),1.2*max(self.load))
-        ax.grid(True)
-        ax.set_xlabel(self.name+": load")
-        
-        
-    def plotbills(self):
-        fig, ax = plt.subplots(1)
-        rects=[]
-        for t in range(len(self.bill)):
-            rect= Rectangle( (t,0), 1, self.bill[t] )
-            rects.append(rect)
-        # Create patch collection with specified colour/alpha
-        pc = PatchCollection(rects, alpha=0.5, facecolor="green", edgecolor="black")
-        # Add collection to axes
-        ax.add_collection(pc)
-        ax.set_xlim(0,len(self.bill))
-        ax.set_ylim(1.2*min(0,min(self.bill)),1.2*max(0,max(self.bill)))
-        ax.grid(True)
-        ax.set_xlabel(self.name+": bill")
-        
-    def plotsupplied(self):
-        fig, ax = plt.subplots(1)
-        rects=[]
-        for t in range(len(self.supplied)):
-            rect= Rectangle( (t,0), 1, self.supplied[t] )
-            rects.append(rect)
-        # Create patch collection with specified colour/alpha
-        pc = PatchCollection(rects, alpha=0.5, facecolor="blue", edgecolor="black")
-        # Add collection to axes
-        ax.add_collection(pc)
-        ax.set_xlim(0,len(self.supplied))
-        ax.set_ylim(1.2*min(0,min(self.supplied)),1.2*max(0,max(self.supplied)))
-        ax.grid(True)
-        ax.set_xlabel(self.name+": supplied load")
-        
-        
+        self.load.append(self.callfunction(t+.5))              
 class Manager:
     def __init__(self, names, callfunctions):
         self.names = names
@@ -79,7 +28,6 @@ class Manager:
         self.max_power= 12
         
     def repartition(self):
-    
         if abs(self.loads[-1])>self.max_power : #on dépasse l'intensité maximale possible
             for name in self.names:
                 player = self.players[name]
@@ -101,28 +49,12 @@ class Manager:
                 else: # pour les consommateurs bill negative
                     player.bill.append( -player.supplied[-1]*self.pe )  #negatif car self.loads negatif
         else: #on importe 
-#warning: (faire attention au max)
             for name in self.names:
                 player = self.players[name]
                 if player.load[-1] <0. : #pour les producteurs bill positive
                     player.bill.append( (-player.supplied[-1]*self.pe ) )
                 else: # pour les consommateurs bill négative
-                    player.bill.append( (player.supplied[-1]/self.loads[-1])*(self.production[-1]*self.pe-self.balance[-1]*self.v) ) # car le balance est négatif              
-    
-    def plotbalance(self):                         
-        fig, ax = plt.subplots(1)
-        rects=[]
-        for t in range(len(self.balance)):
-            rect= Rectangle( (t,0), 1, self.balance[t] )
-            rects.append(rect)
-        # Create patch collection with specified colour/alpha
-        pc = PatchCollection(rects, alpha=0.5, facecolor="orange", edgecolor="black")
-        # Add collection to axes
-        ax.add_collection(pc)
-        ax.set_xlim(0,len(self.balance))
-        ax.set_ylim(1.2*min(0,min(self.balance)),1.2*max(0,max(self.balance)))
-        ax.grid(True)
-        ax.set_xlabel("Global balance")
+                    player.bill.append( (player.supplied[-1]/self.loads[-1])*(self.production[-1]*self.pe-self.balance[-1]*self.v) ) # car le balance est négatif
         
     def simulation(self, T):
         for t in range(T):
@@ -137,22 +69,8 @@ class Manager:
                     self.production[-1] += -player.load[-1] #production positive
             self.balance.append( self.production[-1]+self.loads[-1]) #positif si on exporte et négatif sinon   
             self.repartition()
-        print("\n Total load =")#, self.loads, "\n balance =", self.balance)
-        self.plotbalance()
-        plt.show()
-        for name in self.names:
-            player = self.players[name]
-            print("\n", name)#, "\n load =", player.load, "\n bill =", player.bill)
-            player.plotload()
-            plt.show()
-            
-            player.plotsupplied()
-            plt.show()
-    
-            player.plotbills()
-            plt.show()
-            
-        
+
+## différents types de call
                     
 def gaussiancall(t,avg,sigma,maxi): return(maxi*(mh.e**(-((t-avg)**2)/(2*sigma**2)))/2*mh.sqrt(2*mh.pi))  
 def squarecall(t, inf, sup, maxi): 
@@ -165,10 +83,6 @@ def randomcall(t,avg, sigma):
 
         
 callfunctions={"building": (lambda t: squarecall(t,8,18,10)) , "data_center": (lambda t: randomcall(t,7,3)), "PV_production": (lambda t:gaussiancall(t,12,2,-16)),"VE": (lambda t: randomcall(t,0,5))   }        
-        
-
-
-
 names=[ "building", "data_center", "PV_production", "VE"]
 manager=Manager(names,callfunctions)        
 manager.simulation(24)              
