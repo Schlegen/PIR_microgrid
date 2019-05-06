d01cbeb217fcac0a27fa1e2bd8642e8e3f075f73
BenjiBoze <benji.sauze@hotmail.fr>
2019-05-06 16:11:15 +0200

    Benji modified charging-station

--- players/charging_station.py
+++ players/charging_station.py
@@ -1,17 +1,27 @@
 import numpy as np
+import os 
 
+from numpy.random import randint 
 class ChargingStation:
 
-	def __init__(path_to_data_folder):
+	def __init__(self, path_to_data_folder):
 
 		self.path_to_data_folder = path_to_data_folder
 		self.bill = np.zeros(48)
+		self.scenario = {}
+		self.n_data = 10
 
 
-	def load(time):
+	def load(self,time):
 
 		return 0
 
-	def draw_random_scenario():
-
+	def draw_random_scenario(self):
+		test_load_charging_station =np.loadtxt(os.path.join(self.path_to_data_folder, "charging-station","test_load_vehicles_charging-station.csv"))
+		self.scenario["load_charging_station"]= test_load_charging_station [randint(self.n_data),:]
+		
+		#tableau NPY
+		test_load_charging_station_capacity =np.load(os.path.join(self.path_to_data_folder, "charging-station","test_capacity_power_soc_charging-station.npy"))
+		self.scenario["load_charging_station_capacity"]= test_load_charging_station_capacity [randint(self.n_data),:,:] 
+		
 		return 0
