a8eee0b9d03b04cf9235ad7973a5a72ab6bc58af
unknown <nicolas.begaud@hotmail.fr>
2019-05-07 18:57:52 +0200

    maj

--- players/smart_building.py
+++ players/smart_building.py
@@ -10,6 +10,7 @@ class SmartBuilding:
         self.scenario={}
         self.stock=np.zeros(49)
         self.load=np.zeros(48)
+        self.heat_balance=np.zeros(48)
         
         #données du ballon d'eau chaude
         self.e=0.4
@@ -23,12 +24,12 @@ class SmartBuilding:
         self.delta_t=3600/2
         self.stock[0]=0.25*self.rho*self.V*self.c_p*(self.T_com - self.T_in)
 
-    def compute_load(self, time, heat_exchange):
-        self.load[time]=self.not_flexible(time, heat_exchange) + self.flexible(time, heat_exchange) 
+    def compute_load(self, time):
+        self.load[time]=self.not_flexible(time) + self.flexible(time) 
 
-    def thermic_load(self, time):
+    def call_heat(self):
         
-        prices=np.zeros((6))
+        prices=np.zeros((48,6))
         
         #mettre prices à jour avec le prix unitaire du kW/h aux quantitées : 0;0,2;0,4;0,6;0,8;1 de q_max
         
@@ -36,7 +37,7 @@ class SmartBuilding:
         return(prices)
         
 
-    def flexible(self, time, heat_exchange):
+    def flexible(self, time):
         
         #on demande une certaine énergie pour chauffer le ballon
         
@@ -45,14 +46,14 @@ class SmartBuilding:
         return (thermal_load) #énergie demandée pour chauffer le ballon
 
 
-    def not_flexible(self, time, heat_exchange):
+    def not_flexible(self, time):
         
         load=0
         
         load+=self.scenario['load_smart-building'][t] #demande en énergie domestique
         
         #Evolution du stock d'eau chaude:
-        stock[time+1]=(1-self.r)*stock[time]+heat_exchange-self.scenario['hot_water_demand_smart-building'][t]+(self.COP*self.detla_t*flexible(self,time,heat_exchange))
+        stock[time+1]=(1-self.r)*stock[time]+heat_balance[time]-self.scenario['hot_water_demand_smart-building'][t]+(self.COP*self.detla_t*flexible(self,time,heat_exchange))
         
         if (stock[time+1]<0):#on s'assure qu'il y ait assez d'eau chaude
             load+=(-self.stock[times+1]/(self.delta_t*self.COP))
