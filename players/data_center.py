04ef7b7055815b6949d6923317f4117e63c93282
Schlegen <nicolas.schlegel@eleves.enpc.fr>
2019-05-09 10:29:40 +0200

    seance de microgame 1 : data center

--- players/data_center.py
+++ players/data_center.py
@@ -36,10 +36,10 @@ class DataCenter:
             if (t>=0 and t<12) or t>=44:
                 maxi=0.057/1.98
                 mini=0.06/2.66
-            if (t>=12 and t<16) or (t>=40 and t<41):
+            if (t>=12 and t<16) or (t>=40 and t<44):
                 maxi=0.09/1.98
                 mini=0.10/2.66
-            if (t>=16 and t<44):
+            if (t>=16 and t<40):
                 maxi=0.08/1.98
                 mini=0.1/2.66
             for q in range(6):
